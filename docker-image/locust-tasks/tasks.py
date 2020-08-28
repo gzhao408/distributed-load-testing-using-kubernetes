89a8411fe5afa09b8ba521cc5b317314a1aaf18c
gzhao408 <gloriazhao@google.com>
2020-08-28 13:39:19 -0700

    update tasks to interact with hipster shop

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -15,28 +15,151 @@
 # limitations under the License.
 
 
-import uuid
+# import uuid
 
-from datetime import datetime
-from locust import HttpLocust, TaskSet, task
+# from datetime import datetime
+# from locust import HttpLocust, TaskSet, task
 
 
-class MetricsTaskSet(TaskSet):
-    _deviceid = None
+# class MetricsTaskSet(TaskSet):
+#     _deviceid = None
+
+#     def on_start(self):
+#         self._deviceid = str(uuid.uuid4())
+
+#     @task(1)
+#     def login(self):
+#         self.client.post(
+#             '/login', {"deviceid": self._deviceid})
+
+#     @task(999)
+#     def post_metrics(self):
+#         self.client.post(
+#             "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
+
+
+# class MetricsLocust(HttpLocust):
+#     task_set = MetricsTaskSet
+
+import random
+from locust import HttpLocust, TaskSet
+
+products = [
+    '0PUK6V6EV0',
+    '1YMWWN1N4O',
+    '2ZYFJ3GM2N',
+    '66VCHSJNUP',
+    '6E92ZMYYFZ',
+    '9SIQT8TOJO',
+    'L9ECAV7KIM',
+    'LS4PSXUNUM',
+    'OLJCESPC7Z']
+
+currencies = [
+    'EUR',
+    'USD',
+    'JPY',
+    'GBP',
+    'TRY',
+    'CAD']
+
+# Define specific frontend actions.
+
+def index(l):
+    l.client.get("/")
+
+def setCurrency(l):
+    l.client.post("/setCurrency",
+        {'currency_code': random.choice(currencies)})
+
+def browseProduct(l):
+    l.client.get("/product/" + random.choice(products))
+
+def viewCart(l):
+    l.client.get("/cart")
+
+def emptyCart(l):
+    l.client.post("/cart/empty")
+
+def addToCart(l):
+    product = random.choice(products)
+    l.client.get("/product/" + product)
+    l.client.post("/cart", {
+        'product_id': product,
+        'quantity': random.choice([1,2,3,4,5,10])})
+
+def checkout(l):
+    addToCart(l)
+    l.client.post("/cart/checkout", {
+        'email': 'someone@example.com',
+        'street_address': '1600 Amphitheatre Parkway',
+        'zip_code': '94043',
+        'city': 'Mountain View',
+        'state': 'CA',
+        'country': 'United States',
+        'credit_card_number': '4432-8015-6152-0454',
+        'credit_card_expiration_month': '1',
+        'credit_card_expiration_year': '2039',
+        'credit_card_cvv': '672',
+    })
+
+# LocustIO TaskSet classes defining detailed user behaviors.
+
+class PurchasingBehavior(TaskSet):
 
     def on_start(self):
-        self._deviceid = str(uuid.uuid4())
+        index(self)
 
-    @task(1)
-    def login(self):
-        self.client.post(
-            '/login', {"deviceid": self._deviceid})
+    tasks = {index: 1,
+        setCurrency: 1,
+        browseProduct: 2,
+        addToCart: 2,
+        viewCart: 1,
+        checkout: 1}
 
-    @task(999)
-    def post_metrics(self):
-        self.client.post(
-            "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
+class WishlistBehavior(TaskSet):
 
+    def on_start(self):
+        index(self)
 
-class MetricsLocust(HttpLocust):
-    task_set = MetricsTaskSet
+    tasks = {index: 1,
+        setCurrency: 1,
+        browseProduct: 5,
+        addToCart: 10,
+        viewCart: 5,
+        emptyCart: 2}
+
+class BrowsingBehavior(TaskSet):
+
+    def on_start(self):
+        index(self)
+
+    tasks = {index: 5,
+        setCurrency: 1,
+        browseProduct: 10}
+
+# LocustIO Locust classes defining general user scenarios.
+
+class PurchasingUser(HttpLocust):
+    '''
+    User that browses products, adds to cart, and purchases via checkout.
+    '''
+    task_set = PurchasingBehavior
+    min_wait = 1000
+    max_wait = 10000
+
+class WishlistUser(HttpLocust):
+    '''
+    User that browses products, adds to cart, empties cart, but never purchases.
+    '''
+    task_set = WishlistBehavior
+    min_wait = 1000
+    max_wait = 10000
+
+class BrowsingUser(HttpLocust):
+    '''
+    User that only browses products.
+    '''
+    task_set = BrowsingBehavior
+    min_wait = 1000
+    max_wait = 10000
